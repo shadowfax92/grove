9c83f5ccf0d2341a51bfdc9c5623788142f583b8
Nikhil Sonti <nikhilsv92@gmail.com>
2026-08-19 16:08:30 -0700

    fix: parse Fish boolean flags

--- gv.fish
+++ gv.fish
@@ -1,3 +1,17 @@
+function __gv_flag_enabled
+    set -l name $argv[1]
+    set -l enabled false
+    for argument in $argv[2..]
+        switch $argument
+            case $name "$name=1" "$name=t" "$name=T" "$name=true" "$name=True" "$name=TRUE"
+                set enabled true
+            case "$name=0" "$name=f" "$name=F" "$name=false" "$name=False" "$name=FALSE"
+                set enabled false
+        end
+    end
+    test "$enabled" = true
+end
+
 function gv
     if test (count $argv) -eq 0
         set -l path (grove --null | string split0)
@@ -12,7 +26,7 @@ function gv
     set -l subcommand $argv[1]
     set -l rest $argv[2..]
 
-    if contains -- --help $rest; or contains -- -h $rest; or contains -- --json $rest
+    if contains -- --help $rest; or contains -- -h $rest; or __gv_flag_enabled --json $rest
         grove $argv
         return $status
     end
@@ -33,7 +47,7 @@ function gv
             test -n "$path"
             and builtin cd -- $path
         case rm
-            if contains -- --merged $rest; or contains -- --dry-run $rest; or string match -q -- '--merged=*' $rest; or string match -q -- '--dry-run=*' $rest
+            if __gv_flag_enabled --merged $rest; or __gv_flag_enabled --dry-run $rest
                 grove rm $rest
                 return $status
             end
